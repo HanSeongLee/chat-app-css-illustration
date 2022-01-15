547ea2576c4b502b52ad7c3246379ef37ece91c0
HanSeong Lee <jkhanseong@gmail.com>
2022-01-15 22:52:31 +0900

    ADD | Animation

--- components/BookingApp/index.js
+++ components/BookingApp/index.js
@@ -47,12 +47,18 @@ const Chat = () => {
     return (
         <div className={styles.chat}>
             <ul className={styles.chatList}>
-                <li>
+                <li style={{
+                    '--delay': '1.5s',
+                    '--direction': 'left',
+                }}>
                     <div className={styles.otherChatBubble}>
                         That sounds great. I’d be happy to discuss more.
                     </div>
                 </li>
-                <li>
+                <li style={{
+                    '--delay': '3s',
+                    '--direction': 'left',
+                }}>
                     <div className={styles.otherChatBubble}>
                         Could you send over some pictures of your dog, please?
                     </div>
@@ -60,7 +66,12 @@ const Chat = () => {
 
                 <li></li>
 
-                <li className={styles.me}>
+                <li className={styles.me}
+                    style={{
+                        '--delay': '4.5s',
+                        '--direction': 'right',
+                    }}
+                >
                     <div className={styles.imageContainer}>
                         <img className={styles.image}
                              src={'/img/dog-image-1.jpg'}
@@ -76,12 +87,22 @@ const Chat = () => {
                         />
                     </div>
                 </li>
-                <li className={styles.me}>
+                <li className={styles.me}
+                    style={{
+                        '--delay': '5.5s',
+                        '--direction': 'right',
+                    }}
+                >
                     <div className={styles.meChatBubble}>
                         Here are a few pictures. She’s a happy girl!
                     </div>
                 </li>
-                <li className={styles.me}>
+                <li className={styles.me}
+                    style={{
+                        '--delay': '7s',
+                        '--direction': 'right',
+                    }}
+                >
                     <div className={styles.meChatBubble}>
                         Can you make it?
                     </div>
@@ -89,11 +110,19 @@ const Chat = () => {
 
                 <li></li>
 
-                <li className={styles.otherChatBubble}>
+                <li className={styles.otherChatBubble}
+                    style={{
+                        '--delay': '8.5s',
+                        '--direction': 'left',
+                    }}
+                >
                     She looks so happy! The time we discussed works. How long shall I take her out for?
                 </li>
 
-                <li>
+                <li style={{
+                    '--delay': '9.5s',
+                    '--direction': 'left',
+                }}>
                     <button className={styles.actionButton}>
                         <span className={styles.activity}>
                             30 minute walk
@@ -103,7 +132,10 @@ const Chat = () => {
                         </span>
                     </button>
                 </li>
-                <li>
+                <li style={{
+                    '--delay': '9.5s',
+                    '--direction': 'left',
+                }}>
                     <button className={styles.actionButton}>
                         <span className={styles.activity}>
                             1 hour walk
