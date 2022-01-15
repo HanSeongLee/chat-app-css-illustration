d7611a8999204f3887eafcc0725c8ba0abc9540a
HanSeong Lee <jkhanseong@gmail.com>
2022-01-15 22:23:49 +0900

    ADD | Hero section on desktop

--- components/HeroSection/index.js
+++ components/HeroSection/index.js
@@ -5,15 +5,17 @@ import BookingApp from "../BookingApp";
 const HeroSection = () => {
     return (
         <section className={styles.heroSection}>
-            <BookingApp />
-            <h2 className={styles.title}>
-                Simple booking
-            </h2>
-            <p className={styles.description}>
-                Stay in touch with our dog walkers through the chat interface.
-                This makes it easy to discuss arrangements and make bookings.
-                Once the walk has been completed you can rate your walker and book again all through the chat.
-            </p>
+            <BookingApp/>
+            <div className={styles.content}>
+                <h2 className={styles.title}>
+                    Simple booking
+                </h2>
+                <p className={styles.description}>
+                    Stay in touch with our dog walkers through the chat interface.
+                    This makes it easy to discuss arrangements and make bookings.
+                    Once the walk has been completed you can rate your walker and book again all through the chat.
+                </p>
+            </div>
         </section>
     );
 };
